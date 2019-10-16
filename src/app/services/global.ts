065346d3db94b3e9ac6ca21bf75451ce6420923f
jecontreras68 <joseeduar147gmail.com>
2019-10-15 23:43:56 -0500

    cambios nuevos

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export const GLOBAL = {
-    // url: 'http://localhost:1337/',
-    url: 'https://back-ecomerce.herokuapp.com/',
+     url: 'http://localhost:1337/',
+    //url: 'https://back-ecomerce.herokuapp.com/',
     urlFront: 'http://localhost:8100/'
 }
