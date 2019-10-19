c6aef699616b6182695ac1720129e597cf77983a
jecontreras68 <joseeduar147gmail.com>
2019-10-19 00:33:28 -0500

    cambios de vista de home

--- src/app/component/listproduct/listproduct.component.ts
+++ src/app/component/listproduct/listproduct.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/service-component/producto.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-listproduct',
@@ -10,6 +11,10 @@ export class ListproductComponent implements OnInit {
 
   public list_articulo:any = [];
   public img:any = './assets/imagenes/dilisap1.png';
+  public query:any = {
+    where:{}
+  }
+  public searchtxt:any = '';
   
   constructor(
     private _Producto: ProductoService
@@ -29,9 +34,27 @@ export class ListproductComponent implements OnInit {
     // ]
 
   }
-
+  search(){
+    if(this.searchtxt.length > 1){
+      this.query.where.or = [
+        {
+          titulo:{
+            contains: this.searchtxt || ''
+          }
+        },
+        {
+          slug:{
+            contains: _.kebabCase(this.searchtxt) || ''
+          }
+        }
+      ];
+    }else{
+      delete this.query.where.or;
+    }
+    this.get_producto();
+  }
   get_producto(){
-    return this._Producto.get({})
+    return this._Producto.get(this.query)
     .subscribe((res:any)=>{
       console.log(res);
       this.list_articulo = res;
